b869f8b51325065575e9562b54c12d14f1744b30
stuartelimu <stuartelimu@gmail.com>
2020-11-26 02:53:13 +0300

    add tests for home view

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_page_view_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_page_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
