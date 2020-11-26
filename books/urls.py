3f4ee5ba75328f464547c1ddebde72890f36229f
stuartelimu <stuartelimu@gmail.com>
2020-11-26 03:11:52 +0300

    add url paths

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from .views import BookListView, BookDetailView
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('books/', BookListView.as_view(), name='book_list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
 ]
