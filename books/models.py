aea7f6918eccd62ea2068b3d625b06f670770fbf
stuartelimu <stuartelimu@gmail.com>
2020-11-26 03:11:14 +0300

    create book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book_list', args=[str(self.id)])
